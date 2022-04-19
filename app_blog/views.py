bc0a57de5c5e753bba684742f46b9653dc38d6c2
Hashem Al-qaeadi <hhaasshh79918@gmail.com>
2022-04-19 05:33:13 +0300

    make static folder for all site

--- app_blog/views.py
+++ app_blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home(request):
+    return render(request, 'app_blog/index.html', {'title': 'Home'})
--- pbloggo/settings.py
+++ pbloggo/settings.py
@@ -10,7 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
-from pathlib import Path
+import os
+from pathlib import Path 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -118,6 +119,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+# If you want to make Static folder for all project you can use this code.
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/Users/Eng Hashem Al-qaeadi/Documents/Bootcamp/bloggo/pbloggo/static',
+    
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
